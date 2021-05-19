9ea7576bd34a146b182970214eb73d162ca8c475
TheRealKevBot <Kglas001@gmail.com>
2021-05-19 13:28:03 -0600

    First

---
+++ index.js
@@ -0,0 +1,22 @@
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+const example = {
+    ar: [10, 20, 20, 10, 10, 30, 50, 10, 20],
+    n: 9
+}
+
+function sockMerchant(n, ar) {
+    // Write your code here
+
+} 
+
+sockMerchant(example.n, example.ar)
+
+// console.log(sockMerchant(example.n, example.ar))
